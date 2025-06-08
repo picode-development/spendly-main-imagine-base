7098babbcf2ddca35314b8c509c6ddb32c012cfd
PiCode Development <picode.development@gmail.com>
2025-06-09 00:01:16 +0530

    Update navigation.tsx

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -155,7 +155,7 @@ export const Navigation = () => {
               {/* Content */}
               <div className="p-4 space-y-4">
                 <p className="text-gray-600 dark:text-gray-300 text-sm">
-                  Select how you'd like to interact with your assistant:
+                  Select how you&apos;d like to interact with your assistant:
                 </p>
 
                 {/* Select Dropdown */}
