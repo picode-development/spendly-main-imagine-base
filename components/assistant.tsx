cdd23a9fffe82ea21e89f5c9299ef0806b709890
PiCode Development <picode.development@gmail.com>
2025-06-09 00:00:44 +0530

    Update assistant.tsx

--- components/assistant.tsx
+++ components/assistant.tsx
@@ -70,7 +70,7 @@ const Assistant = () => {
             {/* Content */}
             <div className="p-4 space-y-4">
               <p className="text-gray-300 text-sm">
-                Select how you'd like to interact with your assistant:
+                Select how you&apos;d like to interact with your assistant:
               </p>
 
               {/* Select Dropdown */}
